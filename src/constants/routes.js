14e514d550a164fa752ddcf5f72b9e5c69e83d29
Amit21min <amitmin@live.unc.edu>
2020-09-04 02:04:56 -0400

    adding bulma to packages

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -4,7 +4,6 @@ export const SIGN_IN = '/signin';
 export const HOME = '/home';
 export const ACCOUNT = '/account';
 export const ADMIN = '/admin';
-export const PASSWORD_FORGET = '/pw-forget';
 
 // Sidebar ROUTES
 export const ADMIN_DASHBOARD  = ADMIN+'/dashboard';
