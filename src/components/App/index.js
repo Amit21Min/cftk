1458dd65bcd6dca23687a3d7cc1c84633a5f6236
Amit21min <amitmin@live.unc.edu>
2020-09-04 02:04:56 -0400

    adding bulma to packages

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,7 +3,7 @@ import {
   BrowserRouter as Router,
   Route,
 } from 'react-router-dom';
-
+import 'bulma/css/bulma.css';
 import Navigation from '../Navigation';
 import LandingPage from '../Landing';
 import SignInPage from '../SignIn';
