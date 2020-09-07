1e19ba2558be5cb948ed09fe55d06703aefbd8b5
Lucas Zhang <lhzhang@live.unc.edu>
2020-09-06 22:54:00 -0400

    Most user flow for new route panel implemented

--- src/components/NewRoutePanel/index.js
+++ src/components/NewRoutePanel/index.js
@@ -2,6 +2,11 @@ import React, { useState } from 'react';
 
 const NewRoutePanel = () => {
 
+  // TODO 1: Implement donation, route, and house metrics
+  // TODO 2: Implement revision history and modified by
+  // TODO 3: Deal with google map implementation
+  // TODO 4: Deal with donation amount not necessarily being a number value
+
   const [routeName, setRouteName] = useState('');
   const [currStreet, setCurrStreet] = useState('');
   const [streetNames, setStreetNames] = useState([]);
@@ -10,29 +15,45 @@ const NewRoutePanel = () => {
   const [currNote, setCurrNote] = useState('');
   const [volNotes, setVolNotes] = useState([]);
 
-  const updateInput = (event, setter) => {
-    setter(event.target.value)
-    console.log(event.target.value)
+  const updateInput = (e, setter) => {
+    setter(e.target.value)
+    console.log(e.target.value)
   }
 
-  const updateStreetList = event => {
-    event.preventDefault()
+  const updateStreetList = e => {
+    e.preventDefault()
+    if (streetNames.includes(currStreet)) {
+      alert("Please don't repeat a street name");
+      return;
+    } else if (currStreet === '') {
+      alert("Please enter a street name");
+      return;
+    }
+
     setStreetNames([...streetNames, currStreet]);
     setCurrStreet('');
   }
 
-  const updateNoteList = event => {
-    event.preventDefault()
+  const updateNoteList = e => {
+    e.preventDefault()
+    if (volNotes.includes(currNote)) {
+      alert("Please don't repeat a existing note");
+      return;
+    } else if (currNote === '') {
+      alert("Please enter some text");
+      return;
+    }
+
     setVolNotes([...volNotes, currNote]);
     setCurrNote('');
   }
 
-  const handleDateFocus = event => {
-    event.currentTarget.type = 'date';
+  const handleDateFocus = e => {
+    e.currentTarget.type = 'date';
   }
 
-  const handleDateBlur = event => {
-    event.currentTarget.type = 'text'
+  const handleDateBlur = e => {
+    if (canningDate === '' || canningDate === 'mm/dd/yy') e.currentTarget.type = 'text';
   }
 
   const getHouse = (street, houseNumber) => {
@@ -40,8 +61,28 @@ const NewRoutePanel = () => {
     return `https://www.google.com/maps/embed/v1/search?key=${process.env.REACT_APP_MAPS_API_KEY}&q=${address}`;
   }
 
-  let street = "Hillsborough+Street"; // get from input box, dynamically update and re-render
-  let houseNumber = "425"; // get from input box, dynamically update and re-render
+  const saveForm = e => {
+    if (routeName === '') {
+      alert('Please enter a route name');
+      return;
+    } else if (streetNames.length === 0 && currStreet === '') {
+      alert('Please enter/add a street name');
+      return;
+    }
+
+    console.log({
+      name: routeName,
+      streets: streetNames,
+      date: canningDate,
+      donations: numDonated,
+      notes: volNotes,
+      created: new Date()
+    })
+  }
+
+  // Google map implementation is a placeholder from ViewHouseProperties
+  let street = "Hillsborough+Street";
+  let houseNumber = "425";
   let source = getHouse(street, houseNumber);
 
   return (
@@ -52,51 +93,51 @@ const NewRoutePanel = () => {
           <form>
             <div className="field">
               <div className="control">
-                <input className="input" type="text" placeholder="Name (Required)" value={routeName} onChange={(event) => updateInput(event, setRouteName)} />
+                <input className="input" type="text" placeholder="Name (Required)" value={routeName} onChange={(e) => updateInput(e, setRouteName)} />
               </div>
             </div>
             <h1>Street Name</h1>
             <div>
               {streetNames.map(street => (
-                <div>{street}</div>
+                <div key={street}>{street}</div>
               ))}
             </div>
             <div className="field is-grouped">
               <div className="control">
-                <input className="input" type="text" placeholder="Street Name (Required)" value={currStreet} onChange={(event) => updateInput(event, setCurrStreet)} />
+                <input className="input" type="text" placeholder="Street Name (Required)" value={currStreet} onChange={(e) => updateInput(e, setCurrStreet)} />
               </div>
               <button className="button" onClick={updateStreetList}>Add</button>
             </div>
             <h1>Last Canning Data</h1>
             <div className="field">
               <div className="control">
-                <input className="input" type="text" placeholder="Date (optional)" onFocus={handleDateFocus} onBlur={handleDateBlur} value={canningDate} onChange={(event) => updateInput(event, setCanningDate)} />
+                <input className="input" type="text" placeholder="Date (optional)" onFocus={handleDateFocus} onBlur={handleDateBlur} value={canningDate} onChange={(e) => updateInput(e, setCanningDate)} />
               </div>
               <label className="label">MM/DD/YY</label>
             </div>
             <div className="field">
               <div className="control">
-                <input className="input" type="number" placeholder="Donations (optional)" value={numDonated} onChange={(event) => updateInput(event, setNumDonated)} />
+                <input className="input" type="number" placeholder="Donations (optional)" value={numDonated} onChange={(e) => updateInput(e, setNumDonated)} />
               </div>
             </div>
             <h1>Volunteer Notes</h1>
             <div>
               {volNotes.map(note => (
-                <div>{note}</div>
+                <div key={note}>{note}</div>
               ))}
             </div>
             <div className="field is-grouped">
               <div className="control">
-                <input className="input" type="text" placeholder="Note" value={currNote} onChange={(event) => updateInput(event, setCurrNote)} />
+                <input className="input" type="text" placeholder="Note" value={currNote} onChange={(e) => updateInput(e, setCurrNote)} />
               </div>
               <button className="button" onClick={updateNoteList}>Add</button>
             </div>
           </form>
         </div>
         <div className="column">
-          <div className="google_map">
+          <div>
             <iframe title="viewRoute"
-              width="100"
+              width="600"
               height="450"
               frameBorder="0" styles="border:0"
               src={source}
@@ -104,8 +145,8 @@ const NewRoutePanel = () => {
             </iframe>
           </div>
         </div>
-
       </div>
+      <button className="button" onClick={saveForm}>Save</button>
     </div>
 
   );
