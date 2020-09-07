10294acd1f8a613529ba1276cb9e7e444c038cd6
Lucas Zhang <lhzhang@live.unc.edu>
2020-09-07 18:24:35 -0400

    Added comments to help explain functionality

--- src/components/NewRoutePanel/index.js
+++ src/components/NewRoutePanel/index.js
@@ -6,7 +6,9 @@ const NewRoutePanel = () => {
   // TODO 2: Implement revision history and modified by
   // TODO 3: Deal with google map implementation
   // TODO 4: Deal with donation amount not necessarily being a number value
+  // TODO 5: Figure out required functionality
 
+  // variables used in the state
   const [routeName, setRouteName] = useState('');
   const [currStreet, setCurrStreet] = useState('');
   const [streetNames, setStreetNames] = useState([]);
@@ -16,11 +18,14 @@ const NewRoutePanel = () => {
   const [volNotes, setVolNotes] = useState([]);
 
   const updateInput = (e, setter) => {
+    // Executes the passed in setter, allows for additional functionality afterwards as well
     setter(e.target.value)
     console.log(e.target.value)
   }
 
   const updateStreetList = e => {
+    // Adds street to list as long as street not already included or input is not empty
+    // preventDefault() prevents the page from reloading whenever a button is pressed
     e.preventDefault()
     if (streetNames.includes(currStreet)) {
       alert("Please don't repeat a street name");
@@ -35,6 +40,8 @@ const NewRoutePanel = () => {
   }
 
   const updateNoteList = e => {
+    // Adds note to list as long as the note is not already included or the input is not empty
+    // preventDefault() prevents the page from reloading whenever a button is pressed
     e.preventDefault()
     if (volNotes.includes(currNote)) {
       alert("Please don't repeat a existing note");
@@ -49,19 +56,24 @@ const NewRoutePanel = () => {
   }
 
   const handleDateFocus = e => {
+    // When clicking on input, switches input type to date. Allows for placeholder text
     e.currentTarget.type = 'date';
   }
 
   const handleDateBlur = e => {
+    // Changes input back to text if it is empty so placeholder text can still be shown
     if (canningDate === '' || canningDate === 'mm/dd/yy') e.currentTarget.type = 'text';
   }
 
   const getHouse = (street, houseNumber) => {
+    // Returns link for google maps iframe
     var address = `${houseNumber}+${street}`;
     return `https://www.google.com/maps/embed/v1/search?key=${process.env.REACT_APP_MAPS_API_KEY}&q=${address}`;
   }
 
   const saveForm = e => {
+    // Executes when save button is clicked.
+    // Alerts and doesn't save if required inputs are not filled (Placeholder)
     if (routeName === '') {
       alert('Please enter a route name');
       return;
@@ -70,6 +82,7 @@ const NewRoutePanel = () => {
       return;
     }
 
+    // Currently only logs form information to console
     console.log({
       name: routeName,
       streets: streetNames,
@@ -98,14 +111,17 @@ const NewRoutePanel = () => {
             </div>
             <h1>Street Name</h1>
             <div>
+              {/* List to render street names, starts empty */}
               {streetNames.map(street => (
-                <div key={street}>{street}</div>
+                // Holds street names in buttons, onClick functionality to be dealt with later
+                <button className="button" key={street}>{street}</button>
               ))}
             </div>
             <div className="field is-grouped">
               <div className="control">
                 <input className="input" type="text" placeholder="Street Name (Required)" value={currStreet} onChange={(e) => updateInput(e, setCurrStreet)} />
               </div>
+              {/* Pushes current street to list, then clears input */}
               <button className="button" onClick={updateStreetList}>Add</button>
             </div>
             <h1>Last Canning Data</h1>
@@ -122,12 +138,15 @@ const NewRoutePanel = () => {
             </div>
             <h1>Volunteer Notes</h1>
             <div>
+              {/* List to render volunteer notes, starts empty */}
               {volNotes.map(note => (
-                <div key={note}>{note}</div>
+                // Holds street names in buttons, onClick functionality to be dealt with later
+                <button className="button" key={note}>{note}</button>
               ))}
             </div>
             <div className="field is-grouped">
               <div className="control">
+                {/* Pushes current note to list, then clears input */}
                 <input className="input" type="text" placeholder="Note" value={currNote} onChange={(e) => updateInput(e, setCurrNote)} />
               </div>
               <button className="button" onClick={updateNoteList}>Add</button>
@@ -135,6 +154,7 @@ const NewRoutePanel = () => {
           </form>
         </div>
         <div className="column">
+          {/* Placeholder google map */}
           <div>
             <iframe title="viewRoute"
               width="600"
@@ -146,7 +166,8 @@ const NewRoutePanel = () => {
           </div>
         </div>
       </div>
-      <button className="button" onClick={saveForm}>Save</button>
+      {/* Figuring out submit button, might put into form */}
+      <input className="button" type="submit" onClick={saveForm} value="Save"></input>
     </div>
 
   );
