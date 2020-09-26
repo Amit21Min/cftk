130b8f5fa27c7b57b9bdec6fe63152865f66a5e7
Lucas Zhang <lhzhang@live.unc.edu>
2020-09-25 21:00:34 -0400

    Added more margins to chiplist

--- src/components/ChipList/index.js
+++ src/components/ChipList/index.js
@@ -7,7 +7,7 @@ const ChipList = (props) => {
     return (
         <div>
             {props.list.map(item => (
-                <Chip key={item} color={props.color} label={item} style={{marginRight: 4}}
+                <Chip key={item} color={props.color} label={item} style={{margin: 4}}
                     onClick={props.onClick} onDelete={() => props.onDelete(item)}
                 />
             ))}
