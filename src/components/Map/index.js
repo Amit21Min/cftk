4ba7716e700a20951a06e64694326733d1a8481e
Amit21min <amitmin@live.unc.edu>
2020-10-17 10:46:21 -0400

    limited rerendering

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,10 +1,10 @@
-import { CropPortrait } from "@material-ui/icons";
 import React from "react";
 import { useGoogleMaps } from "react-hook-google-maps";
 
 // based on https://developers.google.com/maps/documentation/javascript/adding-a-google-map
 
 export const Map = React.memo(function Map(props) {
+  console.log(props);
   const { ref, map, google } = useGoogleMaps(
     process.env.REACT_APP_MAPS_API_KEY,
     {
@@ -17,50 +17,27 @@ export const Map = React.memo(function Map(props) {
   if (map) {
     // execute when map object is ready
     var geocoder = new google.maps.Geocoder();
-    var streets = props.streets;
-    var city = props.city;
-    var state = props.state;
     var addresses = props.address;
-
-    // Housenumbers looks like:
-// {
-//     street1: [123,124,125],
-//     street2: [1,2,3,4]
-// }
-
-  var cityState = props.cityState;
-  for (var street in addresses) {
-    console.log(street);
-    console.log(addresses);
-    console.log(addresses[street]);
-    for (var address in addresses[street]) {
-      console.log(addresses[street][address]);
-      var fullAddress = addresses[street][address] + " " + street + "," + cityState;
-      console.log(fullAddress);
-      codeAddress(geocoder, map, fullAddress);
-    }
-
-    // console.log(street);
-    // console.log(address[street]);
-  }
     var cityState = props.cityState;
+
+    for (var street in addresses) {
+      for (var i = 0; i < addresses[street].length; i++) {
+        var fullAddress = addresses[street][i] + " " + street + "," + cityState;
+        codeAddress(geocoder, map, fullAddress);
+      }
+    };
+    
     var iconBase = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/'
     var parkingIcon = iconBase + 'parking_lot_maps.png'
     // address = address.map(address => address + ", " + cityState);
-    
-    // address.forEach(address => codeAddress(geocoder, map, address));
 
     // Most accurate Geocoder result achieved with supplying sa much of the following as possible:
     // House Number, Street Direction, Street Name, Street Suffix, City, State, Zip, Country
   }
 
-  function codeAddress(geocoder, resultsMap, address) {
-    console.log(address);
-    console.log(parkingIcon);
-    
+  function codeAddress(geocoder, resultsMap, address) {    
     geocoder.geocode({ address: address }, (results, status) => {
-      console.log(results[0].geometry.location);
-      if (status === "OK") {
+        if (status === "OK") {
         resultsMap.setCenter(results[0].geometry.location);
         var curmarker = new google.maps.Marker({
           map: resultsMap,
@@ -87,4 +64,10 @@ export const Map = React.memo(function Map(props) {
       <div ref={ref} style={{ width: 400, height: 300 }} />
     </div>
   );
+}, (prevProps, nextProps) => {
+  const lodash = require("lodash");
+  if (lodash.isEqual(prevProps, nextProps)) {
+    return true; // props are equal
+  }
+  return false; // props are not equal -> update the component
 });
