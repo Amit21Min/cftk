ee73a6941063634269711b6bd163ac883c2c23f4
Raghavendra Koppula <54515432+rpranith@users.noreply.github.com>
2020-11-26 18:44:22 -0500

    stylized the navbar and finalizing the sidebar for desktop

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -23,6 +23,7 @@ import Box from '@material-ui/core/Box';
 import * as ROUTES from '../../constants/routes';
 
 import './index.css';
+import logo from '../../assets/images/cftk-logo-white.png';
 
 import { Drawer, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
 import { makeStyles } from "@material-ui/core/styles"
@@ -31,93 +32,103 @@ import RoomIcon from '@material-ui/icons/Room';
 import GroupIcon from '@material-ui/icons/Group';
 import MessageIcon from '@material-ui/icons/Message';
 import SettingsIcon from '@material-ui/icons/Settings';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
 const useStyles = makeStyles((theme) => ({
-  drawerPaper: { 
+  drawerPaper: {
     width: 'inherit',
     background: 'linear-gradient(180deg, #7CC9AA 0%, #0075A3 100%)',
-    // color: 'white'
   },
-  link: { textDecoration: 'none', color: 'white' }
+  link: { 
+    textDecoration: 'none', 
+    color: 'white',
+    '&:hover': {
+      color: 'white'
+    },
+
+  }
 }));
 
 const Sidebar = () => {
   const classes = useStyles();
 
   return (
-  <div id="sidebar">
+    <div id="sidebar">
 
-    <div id="sidebar-menu">
-      <div id="sidebar-adaptation">
+      <div id="sidebar-menu">
+        <div id="sidebar-adaptation">
+          <Switch>
+          </Switch>
+        </div>
+      </div>
+
+      <div id="sidebar-panel">
         <Switch>
+          <Route exact path={ROUTES.ADMIN_DASHBOARD} component={DashboardPanel} />
+          <Route exact path={ROUTES.ADMIN_ROUTES} component={RoutesPanel} />
+          <Route exact path={ROUTES.ADMIN_VOLUNTEERS} component={VolunteersPanel} />
+          <Route exact path={ROUTES.ADMIN_ROUTES_NEW} component={NewRoutePanel} />
+          <Route exact path={ROUTES.ADMIN_ROUTES_DEL} component={DeleteDialogs} />
+          <Route exact path={ROUTES.ADMIN_ROUTES_EDIT} component={EditRoutePanel} />
+          <Route path={ROUTES.ASSIGN_ROUTE} component={AssignRoute} />
+          <Route path={ROUTES.ADMIN} component={AdminPage} />
         </Switch>
       </div>
-    </div>
 
-    <div id="sidebar-panel">
-      <Switch>
-        <Route exact path={ROUTES.ADMIN_DASHBOARD}  component={DashboardPanel} />
-        <Route exact path={ROUTES.ADMIN_ROUTES}     component={RoutesPanel} />
-        <Route exact path={ROUTES.ADMIN_VOLUNTEERS} component={VolunteersPanel} />
-        <Route exact path={ROUTES.ADMIN_ROUTES_NEW} component={NewRoutePanel} />
-        <Route exact path={ROUTES.ADMIN_ROUTES_DEL} component={DeleteDialogs} />
-        <Route exact path={ROUTES.ADMIN_ROUTES_EDIT} component={EditRoutePanel} />
-        <Route path={ROUTES.ASSIGN_ROUTE} component={AssignRoute} />
-        <Route path={ROUTES.ADMIN} component={AdminPage} />
-      </Switch>
-    </div>
-
-    <Drawer
-      style={{width: '200px'}}
-      variant="persistent"
-      anchor="left"
-      open={true}
-      classes={{paper: classes.drawerPaper}}
+      <Drawer
+        style={{ width: '200px' }}
+        variant="persistent"
+        anchor="left"
+        open={true}
+        classes={{ paper: classes.drawerPaper }}
       >
+        <div className="logo-box">
+          <img src={logo}></img>
+        </div>
         <Box
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        p={1}
-        m={1}
-        className="container"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          p={1}
+          m={1}
+          className="container"
         >
           <List>
             <Link to={ROUTES.ADMIN_DASHBOARD} className={classes.link}>
               <ListItem button>
                 <ListItemIcon>
-                  <DashboardIcon className={classes.link}/>
+                  <DashboardIcon className={classes.link} />
                 </ListItemIcon>
-                <ListItemText primary={"Dashboard"}/>
+                <ListItemText primary={"Dashboard"} />
               </ListItem>
             </Link>
             <Link to={ROUTES.ADMIN_ROUTES} className={classes.link}>
               <ListItem button>
                 <ListItemIcon>
-                  <RoomIcon className={classes.link}/>
+                  <RoomIcon className={classes.link} />
                 </ListItemIcon>
-                <ListItemText primary={"Routes"}/>
+                <ListItemText primary={"Routes"} />
               </ListItem>
             </Link>
             <Link to={ROUTES.ADMIN_VOLUNTEERS} className={classes.link}>
               <ListItem button>
                 <ListItemIcon>
-                  <GroupIcon className={classes.link}/>
+                  <GroupIcon className={classes.link} />
                 </ListItemIcon>
-                <ListItemText primary={"Volunteer Groups"}/>
+                <ListItemText primary={"Volunteer Groups"} />
               </ListItem>
             </Link>
             <ListItem button className={classes.link}>
               <ListItemIcon>
-                <MessageIcon className={classes.link}/>
+                <MessageIcon className={classes.link} />
               </ListItemIcon>
-              <ListItemText primary={"Messages"}/>
+              <ListItemText primary={"Messages"} />
             </ListItem>
             <ListItem button className={classes.link}>
               <ListItemIcon>
-                <SettingsIcon className={classes.link}/>
+                <SettingsIcon className={classes.link} />
               </ListItemIcon>
-              <ListItemText primary={"Settings"}/>
+              <ListItemText primary={"Settings"} />
             </ListItem>
             {/* <Link to={ROUTES.LANDING} className={classes.link}>
               <ListItem button>
@@ -144,11 +155,19 @@ const Sidebar = () => {
                   <ListItemText primary={"Admin Page"}></ListItemText>
               </ListItem>
             </Link> */}
-        </List>
+          </List>
         </Box>
-    </Drawer>
-     
-  </div>
+        <div className="logout-container">
+          <ListItem button className={classes.link}>
+            <ListItemIcon>
+              <ExitToAppIcon className={classes.link} />
+            </ListItemIcon>
+            <ListItemText primary={"Logout"} />
+          </ListItem>
+        </div>
+      </Drawer>
+
+    </div>
   );
 };
 
