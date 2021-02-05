544f4e655909945a816e39d3db58b4e4441c13cf
Lucas Zhang <lhzhang@live.unc.edu>
2021-02-05 10:10:08 -0500

    Removed button import in grouped text fields

--- src/components/ReusableComponents/GroupedTextField/DualGroupedTextField.js
+++ src/components/ReusableComponents/GroupedTextField/DualGroupedTextField.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, TextField, Button } from '@material-ui/core';
+import { Grid, TextField } from '@material-ui/core';
 import PillButton from '../PillButton';
 // import ChipList from '../ChipList';
 
