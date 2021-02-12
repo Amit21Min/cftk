e99cb47e47b61ec062700a0b072d152115aa730e
Raghavendra Koppula <54515432+rpranith@users.noreply.github.com>
2021-02-11 23:46:38 -0500

    connected signup form to firebase and added validation for that form

--- src/components/FirebaseComponents/SignUp/index.js
+++ src/components/FirebaseComponents/SignUp/index.js
@@ -8,6 +8,11 @@ import InputAdornment from "@material-ui/core/InputAdornment";
 import Visibility from "@material-ui/icons/Visibility";
 import VisibilityOff from "@material-ui/icons/VisibilityOff";
 import { Button, FilledInput, FormControl, FormHelperText } from "@material-ui/core";
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+
+
+import { auth, db } from "../Firebase/firebase";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -23,7 +28,17 @@ const useStyles = makeStyles((theme) => ({
   container: {
     textAlign: "center",
   },
+  root: {
+    width: '100%',
+    '& > * + *': {
+      marginTop: theme.spacing(2),
+    },
+  },
 }));
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 const App = () => {
   const classes = useStyles();
@@ -38,11 +53,12 @@ const App = () => {
     showConfirmedPassword: false,
   });
 
-  const handleChange = (prop) => (event) => {
-    setValues({ ...values, [prop]: event.target.value });
-  };
+  const[snackBar, setSnackBar] = React.useState({
+    open: false,
+    message: ""
+  })
 
-  const handleConfirmedPasswordChange = (prop) => (event) => {
+  const handleChange = (prop) => (event) => {
     setValues({ ...values, [prop]: event.target.value });
   };
 
@@ -62,6 +78,55 @@ const App = () => {
   const handleMouseDownConfirmedPassword = (event) => {
     event.preventDefault();
   };
+
+  const handleClose = (reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setSnackBar({open: false});
+  };
+
+  const signUp = () => {
+
+    // Validation -----------------------------------------------
+    if(values.email == '' || values.firstName == '' || values.lastName == '') {
+      setSnackBar({open: true, message: "Fill in the values"});
+      return;
+    }
+
+    const email_test = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if(!email_test.test(values.email.toLowerCase())) {
+      setSnackBar({open: true, message: "Email not valid"});
+      return;
+    }
+    
+    if(values.password != values.confirmedPassword) {
+      setSnackBar({open: true, message: 'Passwords do not match'});
+      return;
+    } 
+    
+    if(values.password.length <= 7) {
+      setSnackBar({open: true, message: "Password length too short"});
+      return;
+    }
+
+    // End of Validation ----------------------------------------------
+
+      auth.createUserWithEmailAndPassword(values.email, values.password).then(cred => {
+        return db.collection('User').doc(cred.user.uid).set({
+          firstName: values.firstName,
+          lastName: values.lastName, 
+          email: values.email,
+          phone: null,
+          completedRoutes: [],
+          assignment: null,
+          sms: false,
+          emailNotifications: false
+        })
+      })
+  }
 
   const SignUpButton = withStyles({
     root: {
@@ -102,6 +167,8 @@ const App = () => {
                   label="First Name"
                   autoFocus
                   helperText="Others will be able to see this"
+                  onChange={handleChange('firstName')}
+                  value={values.firstName}
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
@@ -114,6 +181,8 @@ const App = () => {
                   name="lastName"
                   autoComplete="lname"
                   helperText="Others will be able to see this"
+                  onChange={handleChange('lastName')}
+                  value={values.lastName}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -126,6 +195,8 @@ const App = () => {
                   name="email"
                   autoComplete="email"
                   helperText="Others will not be able to see this"
+                  onChange={handleChange('email')}
+                  value={values.email}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -173,7 +244,7 @@ const App = () => {
                     id="standard-adornment-password"
                     type={values.showConfirmedPassword ? "text" : "password"}
                     value={values.confirmedPassword}
-                    onChange={handleConfirmedPasswordChange(
+                    onChange={handleChange(
                       "confirmedPassword"
                     )}
                     endAdornment={
@@ -197,7 +268,12 @@ const App = () => {
             </Grid>
           </form>
           <br></br><br></br>
-          <SignUpButton>Sign-Up</SignUpButton>
+          <SignUpButton onClick={() => signUp()} >Sign-Up</SignUpButton>
+          <Snackbar open={snackBar.open} autoHideDuration={6000} onClose={handleClose}>
+            <Alert onClose={handleClose} severity="error">
+              {snackBar.message}
+            </Alert>
+          </Snackbar>
         </div>
       </div>
     </div>
