aad8fb57d9033ce65c8bd64a6086a0260ec449b5
Lucas Zhang <lhzhang@live.unc.edu>
2021-02-27 18:42:15 -0500

    Adding documentation to AlertSnackbar

--- src/components/ReusableComponents/AlertSnackbar/index.js
+++ src/components/ReusableComponents/AlertSnackbar/index.js
@@ -4,7 +4,10 @@ import { Snackbar } from '@material-ui/core';
 import { Alert } from '@material-ui/lab'
 
 function AlertSnackbar(props) {
-    // Intakes 2 props, the severity
+    // Intakes 2 props, the severity, and the message. 
+    // When either changes, the dialogue will automatically show if the message length is longer than 0 characters
+    // onClose/handleClose is built into this component because personally I don't believe outer components need to worry about closing the dialogue
+
     const [snackBar, setSnackBar] = useState({
         open: false,
         severity: "",
@@ -22,13 +25,14 @@ function AlertSnackbar(props) {
     }
 
     useEffect(() => {
+        // Update the state whenever the severity or message changes
         const isMessage = !!props.message && props.message.length > 0;
         setSnackBar({
             open: isMessage,
             severity: props.severity,
             message: isMessage ? props.message : ""
         })
-    }, [props.chilren, props.severity]);
+    }, [props.message, props.severity]);
 
     return <Snackbar open={snackBar.open} autoHideDuration={6000} onClose={handleClose}>
         <Alert variant="filled" elevation={3} severity={snackBar.severity} onClose={handleClose} style={{ borderRadius: '5em'}}>
