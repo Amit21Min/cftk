ca8ef6549d2885b3ab2939788189ecaf9e43ab94
Lucas Zhang <lhzhang@live.unc.edu>
2021-02-27 18:33:00 -0500

    Created new reusable snackbar component with rounded corners

---
+++ src/components/ReusableComponents/Snackbar/index.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Snackbar } from '@material-ui/core';
+import { Alert } from '@material-ui/lab'
+
+function CustomSnackbar(props) {
+    // Intakes 2 props, the severity
+    const [snackBar, setSnackBar] = useState({
+        open: false,
+        severity: "",
+        message: "",
+    });
+
+    function handleClose(event, reason) {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setSnackBar(prevState => ({
+            ...prevState,
+            open: false
+        }));
+    }
+
+    useEffect(() => {
+        const isMessage = !!props.message && props.message.length > 0;
+        setSnackBar({
+            open: isMessage,
+            severity: props.severity,
+            message: isMessage ? props.message : ""
+        })
+    }, [props.chilren, props.severity]);
+
+    return <Snackbar open={snackBar.open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert variant="filled" elevation={3} severity={snackBar.severity} onClose={handleClose} style={{ borderRadius: '5em'}}>
+            {snackBar.message}
+        </Alert>
+    </Snackbar>
+}
+
+CustomSnackbar.propTypes = {
+    severity: PropTypes.oneOf(['error', 'warning', 'info', 'success']),
+    message: PropTypes.string
+}
+
+export default CustomSnackbar;
