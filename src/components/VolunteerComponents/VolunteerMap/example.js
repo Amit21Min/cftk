ae60ed13f47c470e68a19b2109909ee76d68184b
Lucas Zhang <lhzhang@live.unc.edu>
2021-03-19 18:04:35 -0400

    Added example dialog

--- src/components/VolunteerComponents/VolunteerMap/example.js
+++ src/components/VolunteerComponents/VolunteerMap/example.js
@@ -1,16 +1,17 @@
 import React, { useState } from 'react';
 import VolunteerNavBar from '../VolunteerNavBar';
 import MobileMap from '../../RoutesComponents/Map/mobileMap';
-import { TextField, Paper, IconButton, InputAdornment } from '@material-ui/core';
+import { TextField, Paper, IconButton, InputAdornment, Dialog, DialogTitle, DialogActions, Button } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 
-function VolunteerRoute() {
+function ExampleMap() {
 
     const styleExample = {
         bottom: '0px'
     }
     const [input, setInput] = useState("");
     const [search, setSearch] = useState("");
+    const [addressData, setAddressData] = useState({});
 
     function handleSearch() {
         setSearch(input);
@@ -21,7 +22,7 @@ function VolunteerRoute() {
         setInput(e.target.value);
     }
 
-    function onClick(addressData) {
+    function handleIconClick(addressData) {
         // addressData is an object with 3 fields: key, street, city
         // key holds the house number
         // street holds the street name
@@ -29,12 +30,18 @@ function VolunteerRoute() {
 
         // This example function makes it so that when you click a house Icon, you trigger something
         console.log(addressData);
+        setAddressData(addressData);
+    }
+
+    function handleDialogClose() {
+        // Closes dialog
+        setAddressData({});
     }
 
     return (
         <div style={{ width: '100vw', height: '100vh' }}>
 
-            <MobileMap width={'100%'} height={'calc(100vh - 72px)'} innerStyle={styleExample} routeId={search} onClickIcon={onClick}>
+            <MobileMap width={'100%'} height={'calc(100vh - 72px)'} innerStyle={styleExample} routeId={search} onClickIcon={handleIconClick}>
                 {/* To put a component on top of the map, put it inside the MobileMap component. The innerStyle prop allows for limited styling of inner component */}
                 {/* You can use the absolute positioning to position the element within the map relative to the map itself */}
                 <Paper style={{ width: 'calc(100vw - 20px)', margin: '10px', padding: '10px' }}>
@@ -49,8 +56,15 @@ function VolunteerRoute() {
                 </Paper>
             </MobileMap>
             <VolunteerNavBar tab="route-map"></VolunteerNavBar>
+            {/* This is just a dialog to show that clicking an icon can open something */}
+            <Dialog open={Object.keys(addressData).length > 0}>
+                <DialogTitle>{`${addressData.key} ${addressData.street}, ${addressData.city}`}</DialogTitle>
+                <DialogActions>
+                    <Button onClick={handleDialogClose}>Close</Button>
+                </DialogActions>
+            </Dialog>
         </div>
     )
 }
 
-export default VolunteerRoute;
+export default ExampleMap;
