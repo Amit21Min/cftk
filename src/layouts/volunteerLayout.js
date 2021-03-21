01506215f9d4bda94c7617681119970ac7d85abe
Amit21min <amitmin@live.unc.edu>
2021-03-21 17:13:11 -0400

    added volunteerSettings component to route on the example

--- src/layouts/volunteerLayout.js
+++ src/layouts/volunteerLayout.js
@@ -7,14 +7,14 @@ import {
 } from 'react-router-dom';
 import * as ROUTES from '../constants/routes';
 import ExampleMap from '../components/VolunteerComponents/VolunteerMap/example'
+import VolunteerSettings from '../components/VolunteerComponents/VolunteerSettings';
 
-
-const AdminLayout = () => (
+const VolunteerLayout = () => (
     <Switch>
         <Route exact path={ROUTES.VOLUNTEER_MAP}></Route>
         <Route exact path={ROUTES.VOLUNTEER_MESSAGES}></Route>
         <Route exact path={ROUTES.VOLUNTEER_PROGRESS}></Route>
-        <Route exact path={ROUTES.VOLUNTEER_SETTINGS}></Route>
+        <Route exact path={ROUTES.VOLUNTEER_SETTINGS} component={VolunteerSettings}></Route>
         {/* This is just a placeholder link for the mobile map example. Get rid of it when no one needs it anymore */}
         <Route exact path={'/volunteer/example'} component={ExampleMap}></Route>
     </Switch>
@@ -22,4 +22,4 @@ const AdminLayout = () => (
 
 );
 
-export default AdminLayout;
+export default VolunteerLayout;
