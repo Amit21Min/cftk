e1beb6904c2da0fe5ec6388d75e9cbc3156c35d1
Amit21min <amitmin@live.unc.edu>
2021-03-26 16:08:32 -0400

    added assignment link to volunteer example

--- src/layouts/volunteerLayout.js
+++ src/layouts/volunteerLayout.js
@@ -8,12 +8,13 @@ import {
 import * as ROUTES from '../constants/routes';
 import ExampleMap from '../components/VolunteerComponents/VolunteerMap/example'
 import VolunteerSettings from '../components/VolunteerComponents/VolunteerSettings';
+import VolunteerAssignment from '../components/VolunteerComponents/VolunteerAssignment/index.js';
 
 const VolunteerLayout = () => (
     <Switch>
         <Route exact path={ROUTES.VOLUNTEER_MAP}></Route>
         <Route exact path={ROUTES.VOLUNTEER_MESSAGES}></Route>
-        <Route exact path={ROUTES.VOLUNTEER_PROGRESS}></Route>
+        <Route exact path={ROUTES.VOLUNTEER_PROGRESS} component={VolunteerAssignment}></Route>
         <Route exact path={ROUTES.VOLUNTEER_SETTINGS} component={VolunteerSettings}></Route>
         {/* This is just a placeholder link for the mobile map example. Get rid of it when no one needs it anymore */}
         <Route exact path={'/volunteer/example'} component={ExampleMap}></Route>
