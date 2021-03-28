52f4e7e757a9e18e5197879bc332a018fd1daf50
Raghavendra Koppula <54515432+rpranith@users.noreply.github.com>
2021-03-28 12:45:17 -0400

    starting dashboard

--- src/components/RoutesComponents/DashboardPanel/index.js
+++ src/components/RoutesComponents/DashboardPanel/index.js
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TitleCard from '../../ReusableComponents/TitleCard';
 import ZeroResource from '../../ReusableComponents/ZeroResource';
 import PanelBanner from '../ReusableComponents/PanelBanner';
 
 import './index.css'
 import DashboardNoResource from '../../../assets/images/dashboard-no-resource.png';
+import RouteMetrics from '../ReusableComponents/RouteMetrics';
 
-const DashboardPanel = () => (
+const DashboardPanel = () => {
+
+  let data = true;
+
+  const [routeMetrics, setRouteMetrics] = useState({
+    total_assigned: "0",
+    ready_to_be_assigned: "1",
+    donations_this_year: "$300",
+    delta_from_last_canning: "N/A"
+  });
+
+  return (
   <div>
     <TitleCard title="Dashboard"></TitleCard>
     <div className="dashboard-container">
-        <img src={DashboardNoResource}></img>
-        <br></br>
-        <ZeroResource name="data" msg="Collect data for multiple routes to view a data dashboard."></ZeroResource>
+      {!data && 
+        <div>
+          <img src={DashboardNoResource}></img>
+          <br></br>
+          <ZeroResource name="data" msg="Collect data for multiple routes to view a data dashboard."></ZeroResource>
+        </div>
+      }
+      {data && 
+        <RouteMetrics metrics={routeMetrics}/>
+      }
+
     </div>
   </div>
-);
+  );
+};
 
 export default DashboardPanel;
