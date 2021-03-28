600356efb416ffa427064df17bb52be072abc482
Raghavendra Koppula <54515432+rpranith@users.noreply.github.com>
2021-03-28 17:48:23 -0400

    worked on styling

--- src/components/RoutesComponents/DashboardPanel/index.js
+++ src/components/RoutesComponents/DashboardPanel/index.js
@@ -1,40 +1,70 @@
-import React, { useState } from 'react';
-import TitleCard from '../../ReusableComponents/TitleCard';
-import ZeroResource from '../../ReusableComponents/ZeroResource';
-import PanelBanner from '../ReusableComponents/PanelBanner';
+import React, { useState } from "react";
+import TitleCard from "../../ReusableComponents/TitleCard";
+import ZeroResource from "../../ReusableComponents/ZeroResource";
+import PanelBanner from "../ReusableComponents/PanelBanner";
 
-import './index.css'
-import DashboardNoResource from '../../../assets/images/dashboard-no-resource.png';
-import RouteMetrics from '../ReusableComponents/RouteMetrics';
+import "./index.css";
+import AbstractOne from "../../../assets/images/stat-abstract-1.png"
+import AbstractTwo from "../../../assets/images/stat-abstract-2.png"
+import AbstractThree from "../../../assets/images/stat-abstract-3.png"
+import AbstractFour from "../../../assets/images/stat-abstract-4.png"
+import DashboardNoResource from "../../../assets/images/dashboard-no-resource.png";
 
 const DashboardPanel = () => {
-
   let data = true;
 
-  const [routeMetrics, setRouteMetrics] = useState({
-    total_assigned: "0",
-    ready_to_be_assigned: "1",
-    donations_this_year: "$300",
-    delta_from_last_canning: "N/A"
-  });
-
   return (
-  <div>
-    <TitleCard title="Dashboard"></TitleCard>
-    <div className="dashboard-container">
-      {!data && 
-        <div>
-          <img src={DashboardNoResource}></img>
-          <br></br>
-          <ZeroResource name="data" msg="Collect data for multiple routes to view a data dashboard."></ZeroResource>
-        </div>
-      }
-      {data && 
-        <RouteMetrics metrics={routeMetrics}/>
-      }
-
+    <div>
+      <TitleCard title="Dashboard"></TitleCard>
+      <div>
+        {!data && (
+          <div className="dashboard-container">
+            <img src={DashboardNoResource}></img>
+            <br></br>
+            <ZeroResource
+              name="data"
+              msg="Collect data for multiple routes to view a data dashboard."
+            ></ZeroResource>
+          </div>
+        )}
+        {data && (
+          <div className="data-container">
+            <div className="graph-container">Hello</div>
+            <div className="stat-container">
+              <div className="dashboard-metric-item dashboard-metric-general dashboard-metric-efficient">
+                <img className="abstractOne" src={AbstractOne}></img>
+                <h4 className="dashboard-metric-main">H23</h4>
+                <span className="dashboard-metric-label">
+                  Most Efficient Route
+                </span>
+              </div>
+              <div className="dashboard-metric-item dashboard-metric-general dashboard-metric-learnmore">
+                <img className="abstractTwo" src={AbstractTwo}></img>
+                <h4 className="dashboard-metric-main">M14</h4>
+                <span className="dashboard-metric-label">
+                  Route with Highest Percentage of Residents Interested in
+                  Learning More
+                </span>
+              </div>
+              <div className="dashboard-metric-item dashboard-metric-general dashboard-metric-avgdonation">
+                <img className="abstractThree" src={AbstractThree}></img>
+                <h4 className="dashboard-metric-main">T19</h4>
+                <span className="dashboard-metric-label">
+                  Route with Greatest Average Donation per House
+                </span>
+              </div>
+              <div className="dashboard-metric-item dashboard-metric-general dashboard-metric-totaldonation">
+                <img className="abstractFour" src={AbstractFour}></img>
+                <h4 className="dashboard-metric-main">B28</h4>
+                <span className="dashboard-metric-label">
+                  Route with Greatest Total Donations
+                </span>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
-  </div>
   );
 };
 
