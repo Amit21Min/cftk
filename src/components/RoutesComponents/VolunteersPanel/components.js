0b2b5d93acf5c4f1e4634d06326f0aae74ba7a58
amysong21 <esong21@live.unc.edu>
2021-04-25 17:17:08 -0400

    checkboxes initial fix

--- src/components/RoutesComponents/VolunteersPanel/components.js
+++ src/components/RoutesComponents/VolunteersPanel/components.js
@@ -164,253 +164,6 @@ const useStyles = makeStyles((theme) => ({
 
 }));
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////// NEW GROUP CUSTOM DIALOG //////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-const Transition = React.forwardRef(function Transition(props, ref) {
-  return <Slide direction="up" ref={ref} {...props} />;
-});
-
-// saves the new group to db collecitons
-const newGroup = (groupName, idList) => {
-  
-  db.collection("Groups").doc(groupName).set({
-    assignment: [],
-    users: idList
-  });
-}
-
-
-
-export function FullScreenDialog() {
-  const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
-  const [secondOpen, setSecondOpen] = React.useState(false);
-  const [groupName, setGroupName] = React.useState('');
-
-  const [name, setName] = React.useState('');
-  const [phone, setPhone] = React.useState(0);
-  const [email, setEmail] = React.useState('');
-  const [checked, setChecked] = React.useState(false);
-  const [idList, setIdList] = React.useState([]);
-  const [membersList, setMembers] = React.useState([]);
-  const [id, setId] = React.useState('');
-  const [clicked, setclicks] = React.useState(false);
-
-
-  const Names = [
-    { name: "Tina Smith", phone: '9190000001', email: 'email1@gm.com', id: '001' },
-    { name: "Cameron Ackerman", phone: '9190000002', email: 'email2@gm.com', id: '002' },
-    { name: "Daniel Smith", phone: '9190000003', email: 'email3@gm.com', id: '003' },
-    { name: "Addison Thorton", phone: '9190000004', email: 'email4@gm.com', id: '004' },
-    { name: "Laura Lawson", phone: '9190000005', email: 'email5@gm.com', id: '005' }
-  ]
-
-  const [users, setUsers] = React.useState([]);
-
-  useEffect(() => {
-    db.collection('User').get().then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        //console.log(doc.id, " => ", doc.data());
-        const user=doc.data();
-        user.id=doc.id;
-        setUsers(prevState=> [...prevState,user]);
-        //setIdList(prevState=> [...prevState,doc.id]);
-      })
-      
-    })
-    .catch(function (error) {
-        console.log("error: ", error);
-    })
-
-
-  }, []);
-  
-  
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleSecondClickOpen = () =>{
-    setSecondOpen(true);
-  };
-
-  const handleClose = () => {
-    setMembers([]);
-    setIdList([]);
-    setOpen(false);
-  };
-
-  const handleSecondClose = () => {
-    setSecondOpen(false);
-  };
-  const toggleChecked = () => {
-    setChecked((prev) => !prev);
-    console.log(checked);
-  };
-
-  const findId = (value) => {
-    if (value !== '') {
-      const idFound = users.find(o => o.firstName === value.split(' (')[0].split(' ')[0] && o.lastName===value.split(' (')[0].split(' ')[1]).id;
-      console.log(idFound);
-      setId(idFound);
-      setEmail(users.find(o=>o.id===idFound).email);
-      setPhone(users.find(o=>o.id===idFound).phone);
-    } else {
-      console.log('please select name');
-      setId('');
-    }
-
-  }
-
-  const saveMember = e => {
-    const member={id: id, checked: checked, name: name, email: email, phone: phone}
-    e.preventDefault()
-    if (membersList.includes(member)) {
-      alert("Please don't add an existing member");
-      return;
-    } else if (member === '') {
-      alert("Please select a member to add");
-      return;
-    }
-    console.log(id);
-    console.log(checked);
-    handleSecondClose();
-    setMembers(prevState => [...prevState, member]);
-    setIdList(prevState=> [...prevState, id]);    
-  }
-
-  const saveForm = e => {
-    e.preventDefault()
-    if (membersList.length==0) {
-      handleClose();
-    } else {
-      newGroup(groupName,idList);
-      handleClose();
-      setMembers([]);
-      setIdList([]);
-      setUsers([]);
-    }
-  }
-
-  return (
-    <div>
-      <Button variant="contained" color="primary" onClick={() => {handleClickOpen(); }}>
-        Create New Group
-      </Button>
-      <Dialog fullWidth={'lg'} open={open} onClose={handleClose} TransitionComponent={Transition}>
-        <AppBar className={classes.appBar} color="primary">
-          <Toolbar>
-            <IconButton edge="start" color="inherit" onClick={handleClose} aria-label="close">
-              <CloseIcon />
-            </IconButton>
-            <Typography variant="h6" className={classes.title}>
-              New Group
-            </Typography>
-            <Button autoFocus color="inherit" onClick={saveForm}>
-              save
-            </Button>
-          </Toolbar>
-        </AppBar>
-        <br></br>
-        <TextField required id="standard-required"
-          className={classes.textField}
-          label="Name" size="small"
-          style={{ margin: 8 }}
-          onChange={(e) => { setGroupName(e.target.value); }} />
-        <TextField label="Description" />
-        <br></br>
-        
-        <Grid container className={classes.grid} spacing={2}>
-          
-        <Grid item xs={3} mx='auto' my='auto'>
-        <Card className={classes.card} style={{width:250, height: 250}}>
-          <CardActions  style={{justifyContent: 'center'}}>
-            <Button className={classes.addButton} aria-label="add" variant="outlined" color="primary" onClick={() => {handleSecondClickOpen();}}> 
-              <AddIcon > </AddIcon>
-            </Button>
-            <Dialog fullWidth open={secondOpen}
-              onClose={handleClose}
-              TransitionComponent={Transition}
-              style={{
-                top: 0,
-                left: 0,
-                bottom: 0,
-                right: 0
-              }}>
-              <DialogTitle id="form-dialog-title">New Group Member</DialogTitle>
-              <DialogContent>
-                <Autocomplete
-                  id="name-search"
-                  options={users}
-                  
-                  getOptionSelected={(option, value) => option.firstName === value.firstName && option.lastName===value.lastName}
-                  getOptionLabel={(option) => (option.firstName + ' ' + option.lastName + ' ' + '(' + option.email.split('@')[0] + ')')}
-                  style={{ width: 300 }}
-                  filterSelectedOptions
-                  onInputChange={(event, newValue) => { setName(newValue.split(' (')[0].split(' ')[0]+' '+newValue.split(" (")[0].split(' ')[1]); findId(newValue);}}
-                  renderInput={(params) => <TextField {...params} required label="Name" />}
-                />
-
-                <FormGroup>
-                  <FormControlLabel
-                    control={<Switch checked={checked} onChange={toggleChecked} color="primary" />}
-                    label="Visible to other members"
-                  />
-                </FormGroup>
-              </DialogContent>
-              <DialogActions>
-                <Button onClick={handleSecondClose} color="primary">
-                  Cancel
-                  </Button>
-                <Button onClick={saveMember} color="primary">
-                  Save
-                  </Button>
-              </DialogActions>
-            </Dialog>
-          </CardActions>
-          <CardContent>
-            <Typography variant="h6" component="h2" align='center' style={{ color: "#bdbdbd"}}>
-              Add new member
-            </Typography>
-          </CardContent>
-        </Card>
-      </Grid>
-      
-      {membersList.map(member => (
-        <Grid item xs={3} mx='auto' my='auto' key = {member.id}>
-        <Card className={classes.card} style={{width:250, height: 250}}>
-          <CardContent>
-            <Typography variant="h6" component="h2" align='center'>
-                {member.name}
-            </Typography>
-            <Typography variant="subtitle1" component="h2" align='center'>
-                {member.email}
-            </Typography>
-            <Typography variant="subtitle1" component="h2" align='center'>
-                {member.phone}
-            </Typography>
-          </CardContent>
-          <CardActions style={{justifyContent: 'center'}}>
-            <Button>
-              <EditIcon></EditIcon>
-            </Button>
-            <Button>
-              <DeleteIcon></DeleteIcon>
-            </Button>
-          </CardActions>
-          
-        </Card>
-      </Grid>
-      ))}
-      
-      
-      </Grid>
-      </Dialog>
-    </div>
-  );
-};
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////// GROUP TABLE COMPONENTS /////////////////////////////////////////////////////////////////
@@ -479,13 +232,13 @@ function Row(props) {
   ///// end of alert snack bar
  
   const [checked, setChecked] = React.useState(false);
-  const [userchecked, setCheckedUser] = React.useState(false);
+  const [isChecked, setIsChecked] = React.useState(foundUsers.slice().fill(false));
   const handleChange = (event) => {
     setChecked(event.target.checked);
   };
-  const handleChangeUser=(event) => {
-    setCheckedUser(event.target.checked);
-  }
+  const toggleCheckboxValue = (index) => {
+    setIsChecked(isChecked.map((v, i) => (i === index ? !v : v)));
+}
 
   //const [openMore,setOpenMore]=React.useState(false);
 
@@ -511,7 +264,31 @@ function Row(props) {
     setDeleteDialog(true);
   }
 
-  
+  /// user icon ///
+
+  const [useranchorEl, setuserAnchorEl] = React.useState(null);
+  const openMoreUser = Boolean(useranchorEl);
+
+  const handleMoreClickUser = (event) => {
+    setuserAnchorEl(event.currentTarget);
+  };
+
+  const handleMoreCloseUser = () => {
+    setuserAnchorEl(null);
+  };
+
+
+  const handleEditClickUser =() => {
+    handleMoreCloseUser();
+  }
+
+  const [deleteDialogUser,setDeleteDialogUser] = React.useState(false);
+  const handleDeleteClickUser = () => {
+    handleMoreCloseUser();
+    setDeleteDialogUser(true);
+  }
+
+  ///////////
   
   const handleDeleteGroup = () => {
     const id=row.id
@@ -537,7 +314,11 @@ function Row(props) {
 
     
   }
+////
 
+  const handleDeleteUser = (userid) => {
+    
+  }
   return (
     <React.Fragment>
       <TableRow className={classes.root}>
@@ -568,7 +349,7 @@ function Row(props) {
             onClose={handleMoreClose}>
             <MenuItem onClick={handleEditClick}>Edit
               <Dialog open = {deleteDialog} onClose = {() => setDeleteDialog(false)} >
-                  <DialogTitle id="form-dialog-title">Delete Group</DialogTitle>
+                  <DialogTitle id="form-dialog-title">Edit Group</DialogTitle>
                   <DialogContent>
                     <DialogContentText>
                       Do you want to delete group: {row.id}?
@@ -585,7 +366,7 @@ function Row(props) {
                             message: msg.message
                           })
                         });}} color="primary">
-                    Delete
+                    Save
                   </Button>
                   </DialogActions>
               </Dialog>
@@ -629,17 +410,17 @@ function Row(props) {
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {foundUsers.map((user) => (
+                  {foundUsers.map((user,index) => (
                     
                     
                     <TableRow key={user.id}>
                       <TableCell padding="checkbox"> 
                           
                           <Checkbox
-                            checked={userchecked}
-                            onChange={(event) => {
-                              setCheckedUser(event.target.checked);
-                            }}
+                            key={index}
+                            checked={isChecked[index]}
+                            onClick={() => toggleCheckboxValue(index)}
+                            
                             inputProps={{ 'aria-label': 'primary checkbox' }}
                           />
                       </TableCell>
@@ -650,7 +431,38 @@ function Row(props) {
                       <TableCell align="right"  size = "medium" >{user.phone}</TableCell>
                       <TableCell align="right" size = "medium">{user.email}</TableCell>
                       <TableCell align="right" size = "small">{user.emailNotifications ? <EmailIcon color="primary"/> : <MailOutlineIcon color="#e0e0e0"/>} {user.sms ? <MessageIcon color="primary"/> : <SpeakerNotesOffOutlinedIcon color="#e0e0e0"/>} </TableCell>
-
+                      <TableCell align="right">
+                          <IconButton onClick={handleMoreClickUser}>
+                            <MoreVertIcon />
+                          </IconButton>
+                          <Menu 
+                            anchorEl={useranchorEl}
+                            keepMounted
+                            open={openMoreUser}
+                            onClose={handleMoreCloseUser}>
+                            <MenuItem onClick={handleEditClickUser}>Edit
+                            
+                            </MenuItem>
+                            <MenuItem onClick={handleDeleteClickUser}>Delete</MenuItem>
+                              <Dialog open = {deleteDialogUser} onClose = {() => setDeleteDialogUser(false)} >
+                                <DialogTitle id="form-dialog-title">Delete Group</DialogTitle>
+                                <DialogContent>
+                                  <DialogContentText>
+                                    Do you want to delete member: {user.firstName} {user.lastName}?
+                                  </DialogContentText>
+                                </DialogContent>
+                                <DialogActions>
+                                <Button onClick={() => setDeleteDialogUser(false)} color="secondary">
+                                  Cancel
+                                </Button>
+                                <Button onClick={handleDeleteUser(user.id)} color="primary">
+                                  Delete
+                                </Button>
+                                </DialogActions>
+                              </Dialog>
+                          </Menu>
+                        </TableCell>
+                     
                     </TableRow>
                   ))}
                 </TableBody>
@@ -847,8 +659,12 @@ export const GroupTable = (props) => {
         {}
         <TableHead>
           <TableRow>
-            <TableCell padding="checkbox"> 
-            
+              <TableCell padding="checkbox"> 
+                <Checkbox
+                  checked={checked}
+                  onChange={handleChange}
+                  inputProps={{ 'aria-label': 'primary checkbox' }}
+                />
             </TableCell>
 
             <TableCell size="small"/>
@@ -863,7 +679,11 @@ export const GroupTable = (props) => {
         {(rowsPerPage > 0
             ? data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
             : data).map((row) => (
-            <Row key={row.id} row={row} />
+              
+            <Row key={row.id} row={row}>
+              
+            </Row>
+            
           ))}
           {emptyRows > 0 && (
             <TableRow style={{ height: 53 * emptyRows }}>
