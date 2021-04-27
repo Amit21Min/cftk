5d13af023778211022a2ab6944230234005bd99a
Lucas Zhang <lhzhang@live.unc.edu>
2021-04-27 10:33:00 -0400

    Fixed abstract image assignments for routemetrics

--- src/components/RoutesComponents/ReusableComponents/RouteMetrics/index.js
+++ src/components/RoutesComponents/ReusableComponents/RouteMetrics/index.js
@@ -12,20 +12,20 @@ const RouteMetrics = (props) => {
   return (
     <div className="metric-list-container">
       <div className='route-metric-assign route-metric-general'>
-        <img src={AbstractOne} alt="" className="route-metric-abstract"></img>
+        <img src={AbstractTwo} alt="" className="route-metric-abstract"></img>
         <MetricItem metric={props.metrics.total_assigned} label="Routes Assigned" />
       </div>
 
       <div className='route-metric-ready route-metric-general'>
-        <img src={AbstractTwo} alt="" className="route-metric-abstract"></img>
+        <img src={AbstractThree} alt="" className="route-metric-abstract"></img>
         <MetricItem metric={props.metrics.ready_to_be_assigned} label="Routes Ready to be Assigned" />
       </div>
       <div className='route-metric-donations route-metric-general'>
-        <img src={AbstractThree} alt="" className="route-metric-abstract"></img>
+        <img src={AbstractFour} alt="" className="route-metric-abstract"></img>
         <MetricItem metric={props.metrics.donations_last_event} label="Latest Event Donation Amount" />
       </div>
       <div className='route-metric-gain route-metric-general'>
-        <img src={AbstractFour} alt="" className="route-metric-abstract"></img>
+        <img src={AbstractOne} alt="" className="route-metric-abstract"></img>
         <MetricItem metric={props.metrics.donations_from_year} label="One-year Total Donation Amount" />
       </div>
     </div>
